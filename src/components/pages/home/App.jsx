ff77f8ea80740a62664d2f41858a9850178ccf8a
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2022-11-09 10:58:03 -0300

    bibliotecas instaladas

--- src/components/pages/home/App.jsx
+++ src/components/pages/home/App.jsx
@@ -1,87 +1,64 @@
 import './App.css'; 
 
+import promo1 from '../../../imagens/promo1.png';
+
+import local from '../../../imagens/local.jpg';
+
+import prod1 from '../../../imagens/temp/hamburger-bacon.jpg';
+import prod2 from '../../../imagens/temp/hamburger-batata.jpg';
+import prod3 from '../../../imagens/temp/lanche1.jpg';
+import prod4 from '../../../imagens/temp/suco2.jpg';
+import prod5 from '../../../imagens/temp/suco-laranja.jpg';
+import prod6 from '../../../imagens/temp/sorvete.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      <header>
-        {/* Logo e navegação */}
-        <nav className="containerNav">
-            <div className="menu">
-                <div>
-                    <span className="material-icons icon" id="logo">
-                        fastfood
-                    </span>        
-                    <label for="" id="titulo">BomBurguer</label>
-                </div>                      
-                <div className="menuGrande">
-                    <a href="#" className="active">Home</a>                  
-                    <a href="./paginas/produtos.html">Produtos</a>
-                    <a href="./paginas/cadUsuario.html">Cadastrar</a>
-                    <a href="./paginas/contato.html">Contato</a>
-                    <a href="./paginas/login.html">Login</a>
+
+        <div className="slider">
+
+            <div className="slides">
+
+                <input type="radio" name="radio-btn" id="radio1" />
+                <input type="radio" name="radio-btn" id="radio2" />
+                <input type="radio" name="radio-btn" id="radio3" />
+                <input type="radio" name="radio-btn" id="radio4" />
+
+                <div className="slide primeira">
+                    <img src={promo1} alt="Promoção 1" />
                 </div>
-                <div className="menuMobile">
-                    <a href="javascript:void(0);" className="icon" id="mIco">
-                        <span className="material-icons icon" id="menu">
-                            menu
-                        </span>
-                    </a>
-                </div>                    
-            </div>  
-            <div className="menuMobileExpandidon" id="mostraOpMobile">  
-                <a href="#" class="active">Home</a>                  
-                <a href="./paginas/produtos.html">Produtos</a>
-                <a href="./paginas/cadUsuario.html">Cadastrar</a>
-                <a href="./paginas/contato.html">Contato</a>
-                <a href="./paginas/login.html">Login</a>
-            </div>               
-        </nav>            
-      </header>
+                <div className="slide">
+                    <img src="./imagens/promo2.png" alt="Promoção 2" />
+                </div>
+                <div className="slide">
+                    <img src="./imagens/promo3.png" alt="Promoção 3" />
+                </div>
+                <div className="slide">
+                    <img src="./imagens/promo4.png" alt="Promoção 4" />
+                </div>
 
-    <div className="slider">
+                <div className="navigation-auto">
+                    <div className="auto-btn1"></div>
+                    <div className="auto-btn2"></div>
+                    <div className="auto-btn3"></div>
+                    <div className="auto-btn4"></div>
+                </div>
 
-        <div className="slides">
-
-            <input type="radio" name="radio-btn" id="radio1" />
-            <input type="radio" name="radio-btn" id="radio2" />
-            <input type="radio" name="radio-btn" id="radio3" />
-            <input type="radio" name="radio-btn" id="radio4" />
-
-            <div className="slide primeira">
-                <img src="./imagens/promo1.png" alt="Promoção 1" />
             </div>
-            <div className="slide">
-                <img src="./imagens/promo2.png" alt="Promoção 2" />
-            </div>
-            <div className="slide">
-                <img src="./imagens/promo3.png" alt="Promoção 3" />
-            </div>
-            <div className="slide">
-                <img src="./imagens/promo4.png" alt="Promoção 4" />
-            </div>
-
-            <div className="navigation-auto">
-                <div className="auto-btn1"></div>
-                <div className="auto-btn2"></div>
-                <div className="auto-btn3"></div>
-                <div className="auto-btn4"></div>
+            
+            <div className="manual-navigation">
+                <label for="radio1" className="manual-btn"></label>
+                <label for="radio2" className="manual-btn"></label>
+                <label for="radio3" className="manual-btn"></label>
+                <label for="radio4" className="manual-btn"></label>                
             </div>
 
         </div>
-        
-        <div className="manual-navigation">
-            <label for="radio1" className="manual-btn"></label>
-            <label for="radio2" className="manual-btn"></label>
-            <label for="radio3" className="manual-btn"></label>
-            <label for="radio4" className="manual-btn"></label>                
-        </div>
-
-    </div>
 
         <main className="principal">
             {/* Info local */}
-            <img className="imagemLocal" src="./imagens/local.jpg" alt="Imagem do local" />
+            <img className="imagemLocal" src={local} alt="Imagem do local" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quasi consequuntur quas numquam ullam fuga voluptas? Sunt, soluta beatae fugit ipsa eos quam ratione explicabo voluptates blanditiis, suscipit repellendus voluptatem. Hic quasi consequuntur quas numquam ullam fuga voluptas? Sunt, soluta beatae fugit ipsa eos quam ratione explicabo voluptates blanditiis, suscipit repellendus voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quasi consequuntur quas numquam ullam fuga voluptas! Hic quasi consequuntur quas numquam ullam fuga voluptas...
             </p>
             
@@ -109,49 +86,37 @@ function App() {
         <div className="produtos">
             {/* Alguns produtos */}
             <div className="cardProd">
-                <img src="../../imagens/temp/hamburger-bacon.jpg" alt="" />
+                <img src={prod1} alt="" />
                 <span className="descricao">Lanche</span>
                 <span>R$ 0,00</span>
             </div>
             <div className="cardProd">
-                <img src="./imagens/temp/hamburger-batata.jpg" alt="" />
+                <img src={prod2} alt="" />
                 <span className="descricao">Lanche</span>
                 <span>R$ 0,00</span>
             </div>
             <div className="cardProd">
-                <img src="./imagens/temp/lanche1.jpg" alt="" />
+                <img src={prod3} alt="" />
                 <span className="descricao">Lanche</span>
                 <span>R$ 0,00</span>
             </div>
             <div className="cardProd">
-                <img src="./imagens/temp/suco2.jpg" alt="" />
+                <img src={prod4} alt="" />
                 <span className="descricao">Lanche</span>
                 <span>R$ 0,00</span>
             </div>
             <div className="cardProd">
-                <img src="./imagens/temp/suco-laranja.jpg" alt="" />
+                <img src={prod5} alt="" />
                 <span className="descricao">Lanche</span>
                 <span>R$ 0,00</span>
             </div>
             <div className="cardProd">
-                <img src="./imagens/temp/sorvete.jpg" alt="" />
+                <img src={prod6} alt="" />
                 <span className="descricao">Lanche</span>
                 <span>R$ 0,00</span>
             </div>
         </div>
 
-        <footer className="rodape">
-            {/* Dados local */}
-            <div className="social">
-                <img src="./imagens/icones/facebook.svg" alt="facebook" />
-                <img src="./imagens/icones/telegram.svg" alt="" />
-                <img src="./imagens/icones/whatsapp.svg" alt="" />
-                <img src="./imagens/icones/tiktok.svg" alt="" />
-            </div>
-            <p>Lanches BomNurguer de Cidade ME | 00.000.000/0000-00</p>
-            <p>Rua Brasil, 1000 - centro - Parapuã/SP | bbgr@bbuguer.com</p>
-        </footer>
-        
         <script src='scripts/nav.js'></script>
         <script src='scripts/home.js'></script>      
     </div>
